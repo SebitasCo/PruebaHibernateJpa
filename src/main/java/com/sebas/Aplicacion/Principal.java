1fd727310dc57e5b02f90aba72b5421ad0e64daf
jllxno <juanllxno201@gmail.com>
2025-04-24 21:56:20 -0500

    Initial commit

---
+++ src/main/java/com/sebas/Aplicacion/Principal.java
@@ -0,0 +1,8 @@
+package com.sebas.Aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        Aplicacion miAplicacion=new Aplicacion();
+        miAplicacion.iniciar();
+    }
+}
